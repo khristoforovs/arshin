44fcff1861cfe1eca31e17da97b8c404e3cdd0ab
Stanislav Khristoforov <stnslv91@gmail.com>
2025-08-11 14:39:12 +0300

    - re-exports for user convenience

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,22 @@ pub mod quantities;
 pub mod registry;
 pub mod transformations;
 pub mod units;
+
+pub use errors::ArshinError;
+pub use transformations::{
+    MathOpsF64,
+    UnitTransformation,
+    LinearTransformation,
+    IdentityTransformation,
+    DecibelTransformation,
+};
+pub use fundamentals::{
+    Fundamentals,
+    FUNDAMENTALS_NUMBER,
+    base,
+    Dimension,
+};
+pub use units::Unit;
+pub use quantities::Quantity;
+pub use registry::{UnitRegistry, DEFAULT_REGISTRY};
+pub use parser::parse_units_file;
